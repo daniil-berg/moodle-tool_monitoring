47fe458b4c42c3d42a5bcdbe1af495e546f6fc8b
Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
2025-09-02 14:11:10 +0200

    Initial commit

---
+++ settings.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin administration pages are defined here.
+ *
+ * @package     tool_monitoring
+ * @category    admin
+ * @copyright   2025 MootDACH DevCamp
+ *              Daniel Fainberg <d.fainberg@tu-berlin.de>
+ *              Martin Gauck <martin.gauk@tu-berlin.de>
+ *              Sebastian Rupp <sr@artcodix.com>
+ *              Malte Schmitz <mal.schmitz@uni-luebeck.de>
+ *              Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage('tool_monitoring_settings', new lang_string('pluginname', 'tool_monitoring'));
+
+    // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
+    if ($ADMIN->fulltree) {
+        // TO-DO: Define actual plugin settings page and add it to the tree - {@link https://docs.moodle.org/dev/Admin_settings}.
+    }
+}

9f3f9ce0c3697915e640270675cb1079ea89dbc8
Daniel Fainberg <d.fainberg@tu-berlin.de>
2026-04-23 20:22:24 +0200

    test: add unit tests for the `privacy\provider` class

---
+++ tests/privacy/provider_test.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Definition of the {@see provider_test} class.
+ *
+ * @package    tool_monitoring
+ * @copyright  2025 MootDACH DevCamp
+ *             Daniel Fainberg <d.fainberg@tu-berlin.de>
+ *             Martin Gauk <martin.gauk@tu-berlin.de>
+ *             Sebastian Rupp <sr@artcodix.com>
+ *             Malte Schmitz <mal.schmitz@uni-luebeck.de>
+ *             Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * {@noinspection PhpIllegalPsrClassPathInspection}
+ */
+
+namespace tool_monitoring\privacy;
+
+use advanced_testcase;
+use core_privacy\local\metadata\null_provider;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * Unit tests for the {@see provider} class.
+ *
+ * @package    tool_monitoring
+ * @copyright  2025 MootDACH DevCamp
+ *             Daniel Fainberg <d.fainberg@tu-berlin.de>
+ *             Martin Gauk <martin.gauk@tu-berlin.de>
+ *             Sebastian Rupp <sr@artcodix.com>
+ *             Malte Schmitz <mal.schmitz@uni-luebeck.de>
+ *             Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+#[CoversClass(provider::class)]
+final class provider_test extends advanced_testcase {
+    public function test_get_reason(): void {
+        self::assertTrue(is_subclass_of(provider::class, null_provider::class));
+        $stringid = provider::get_reason();
+        self::assertTrue(get_string_manager()->string_exists($stringid, 'tool_monitoring'));
+    }
+}
